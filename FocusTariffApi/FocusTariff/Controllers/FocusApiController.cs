bb6b8b693198fdbe541bf0837103c5e4db136de1
merwin2316 <38317917+merwin2316@users.noreply.github.com>
2024-11-23 11:12:56 +0530

    Project API Intialized

---
+++ FocusTariffApi/FocusTariff/Controllers/FocusApiController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FocusTariff.Models;
+
+namespace FocusTariff.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FocusApiController : ControllerBase
+    {
+        private readonly FocusDbContext _context;
+
+        public FocusApiController(FocusDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FocusApi
+        [HttpGet]
+        public ActionResult<IEnumerable<HSCodes>> Get()
+        {
+            return _context.HSCodes.ToList();
+        }
+
+        // GET: api/FocusApi/85
+        [HttpGet("{id}")]
+        public ActionResult<HSCodes> Get(int id)
+        {
+            var hsCode = _context.HSCodes.FirstOrDefault(x => x.Chapter == id);
+            if (hsCode == null)
+                return NotFound();
+
+            return hsCode;
+        }
+
+        //Get api/FocusApi/hs/hs_code
+        [HttpGet("hs/{code}")]
+        public ActionResult<HSCodes> GetByHSCode(string code)
+        {
+            var hsCode = _context.HSCodes.FirstOrDefault(x => x.HS_Code == code);
+            if (hsCode == null)
+                return NotFound();
+
+            return hsCode;
+        }
+
+
+    }
+}
---
+++ FocusTariffApi/FocusTariff/FocusDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FocusTariff.Models;
+
+public class FocusDbContext : DbContext
+{
+    public FocusDbContext(DbContextOptions<FocusDbContext> options) : base(options) { }
+
+    public DbSet<HSCodes> HSCodes { get; set; }
+}
