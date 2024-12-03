f848885e9ab13dcf3f0604667e720ad6e958af0b
baladotdextserv <bala.dextserv@gmail.com>
2024-12-03 12:06:02 -0800

    Add Cors in Program.cs and Modified .gitignore

--- CBVanguardApi/CBVanguardApi.WebApi/Program.cs
+++ CBVanguardApi/CBVanguardApi.WebApi/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddSwaggerGen();
 // Add Infrastructure Layer
 builder.Services.AddPersistenceInfrastructure(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -16,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
